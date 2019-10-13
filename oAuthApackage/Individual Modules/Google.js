9437b3ca4d613bf36de296a509dad5b7e57d8b7a
Ben Ray <benray887@gmail.com>
2019-10-13 12:03:45 -0700

    adding the npm module

---
+++ oAuthApackage/Individual Modules/Google.js
@@ -0,0 +1,11 @@
+let oGoogle = {}
+
+//googleLogIn requires the Client ID, the Client Secret, and the Redirect URL
+//as inputs
+oGoogle.googleLogIn = (test) => {
+    console.log(test, 'logged into google')
+}
+
+module.exports = oGoogle
+
+
