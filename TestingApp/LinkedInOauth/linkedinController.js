486a7065dbcdfa5b533dccba53a84647f69f0cc7
Tom Herrmann <tomherrmanndev@gmail.com>
2019-10-14 16:09:51 -0400

    dunzo

--- TestingApp/LinkedInOauth/linkedinController.js
+++ TestingApp/LinkedInOauth/linkedinController.js
@@ -3,9 +3,45 @@ const app = express();
 const request = require('superagent');
 
 module.exports = {
-  linkedin: (id, secret, userAgent) => {
+  linkedinLogin: (id, secret, redirect, state) => {
     return (req, res, next) => {
-      console.log('REQUEST', req);
+      request
+        .get(
+          `https://www.linkedin.com/oauth/v2/authorization?response_type=code&client_id=${id}&redirect_uri=${redirect}&state=${state}&scope=r_liteprofile%20r_emailaddress%20w_member_social`
+        )
+        .then(data => {
+          res.redirect(data.redirects);
+          return next();
+        })
+        .catch(err => {
+          if (err) res.status(500).sent('SERVER ERROR');
+          console.log('LOGIN GET REQUREST ERROR');
+        });
+    };
+  },
+
+  linkedinToken: (id, secret, redirect, state) => {
+    return (req, res, next) => {
+      const reqCode = req.query.code;
+      const reqState = req.query.state;
+      if (state !== reqState) {
+        res.status(500).send('Server Error');
+        return next();
+      }
+      request
+        .post('https://www.linkedin.com/oauth/v2/accessToken')
+        .set('Content-Type', 'application/x-www-form-urlencoded')
+        .send(
+          `grant_type=authorization_code&code=${reqCode}&redirect_uri=${redirect}&client_id=${id}&client_secret=${secret}`
+        )
+        .then(data => {
+          const access_token = data.body.access_token;
+          request
+            .get('https://api.linkedin.com/v2/me')
+            .set('Connection', 'Keep-Alive')
+            .set('Authorization', `Bearer ${access_token}`)
+            .then(data => console.log(data));
+        });
     };
   }
 };
